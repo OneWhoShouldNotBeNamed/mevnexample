2250d13d2ac033d48f7a0f0d36cc95dfd6c024dc
Mahesh SK <maheshsk019@gmail.com>
2019-07-07 23:59:51 +0530

    Commit before login and JWT

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
+
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+      </ul>
+    </nav><br />
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HomeComponent from './components/HomeComponent.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app', 
+   /*components: {
+    HomeComponent
+  }*/
 }
 </script>
 
@@ -25,4 +40,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
 </style>
